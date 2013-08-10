6e9878bb11ebc94eb5ee5e404fcd600d72cf3a2d
Jan Antala <jan@antala.sk>
2013-08-10 23:38:00 +0200

    update test configuration, bump version

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,11 +1,14 @@
-basePath = '..';
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'src/adaptive-speech.js',
-  'test/*.spec.js'
-];
-singleRun = true;
-browsers = [ 'Chrome' ];
+module.exports = function(config) {
+  config.set({
+    basePath: '..',
+    files: [
+      'components/angular/angular.js',
+      'components/angular-mocks/angular-mocks.js',
+      'src/adaptive-speech.js',
+      'test/*.spec.js'
+    ],
+    frameworks: ['jasmine'],
+    singleRun: true,
+    browsers: [ 'Chrome' ]
+  });
+};
