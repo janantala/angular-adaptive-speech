88d5a652b73d5923908ab8cf13f5f580dc13da1a
Jan Antala <jan@antala.sk>
2014-01-24 22:30:49 +0100

    style(demo): indent using spaces

--- demo/js/controllers/todoCtrl.js
+++ demo/js/controllers/todoCtrl.js
@@ -8,27 +8,27 @@
  * - exposes the model to the template and provides event handlers
  */
 todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage, filterFilter, $speechSynthetis, $speechRecognition) {
-	var todos = $scope.todos = todoStorage.get();
+  var todos = $scope.todos = todoStorage.get();
 
-	$scope.newTodo = '';
-	$scope.remainingCount = filterFilter(todos, {completed: false}).length;
-	$scope.editedTodo = null;
+  $scope.newTodo = '';
+  $scope.remainingCount = filterFilter(todos, {completed: false}).length;
+  $scope.editedTodo = null;
 
-	if ($location.path() === '') {
-		$location.path('/');
-	}
+  if ($location.path() === '') {
+    $location.path('/');
+  }
 
-	$scope.location = $location;
+  $scope.location = $location;
 
-	$scope.$watch('location.path()', function (path) {
+  $scope.$watch('location.path()', function (path) {
     $scope.statusFilter = (path === '/active') ?
-			{ completed: false } : (path === '/completed') ?
-			{ completed: true } : null;
-	});
+      { completed: false } : (path === '/completed') ?
+      { completed: true } : null;
+  });
 
-	$scope.$watch('remainingCount == 0', function (val) {
-		$scope.allChecked = val;
-	});
+  $scope.$watch('remainingCount == 0', function (val) {
+    $scope.allChecked = val;
+  });
 
   $scope.safeApply = function(fn) {
     var phase = this.$root.$$phase;
@@ -41,91 +41,91 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage,
     }
   };
 
-	$scope.addTodo = function () {
-		var newTodo = $scope.newTodo.trim();
-		console.log(newTodo);
-		if (newTodo.length === 0) {
-			return;
-		}
+  $scope.addTodo = function () {
+    var newTodo = $scope.newTodo.trim();
+    console.log(newTodo);
+    if (newTodo.length === 0) {
+      return;
+    }
 
-		todos.push({
-			title: newTodo,
-			completed: false
-		});
-		todoStorage.put(todos);
-		console.log(todos);
-		$scope.newTodo = '';
-		$scope.remainingCount++;
-	};
+    todos.push({
+      title: newTodo,
+      completed: false
+    });
+    todoStorage.put(todos);
+    console.log(todos);
+    $scope.newTodo = '';
+    $scope.remainingCount++;
+  };
 
-	$scope.editTodo = function (todo) {
-		$scope.editedTodo = todo;
-	};
+  $scope.editTodo = function (todo) {
+    $scope.editedTodo = todo;
+  };
 
-	$scope.doneEditing = function (todo) {
-		$scope.editedTodo = null;
-		todo.title = todo.title.trim();
+  $scope.doneEditing = function (todo) {
+    $scope.editedTodo = null;
+    todo.title = todo.title.trim();
 
-		if (!todo.title) {
-			$scope.removeTodo(todo);
-		}
+    if (!todo.title) {
+      $scope.removeTodo(todo);
+    }
 
-		todoStorage.put(todos);
-	};
+    todoStorage.put(todos);
+  };
 
-	$scope.removeTodo = function (todo) {
-		$scope.remainingCount -= todo.completed ? 0 : 1;
-		todos.splice(todos.indexOf(todo), 1);
-		todoStorage.put(todos);
-	};
+  $scope.removeTodo = function (todo) {
+    $scope.remainingCount -= todo.completed ? 0 : 1;
+    todos.splice(todos.indexOf(todo), 1);
+    todoStorage.put(todos);
+  };
 
-	$scope.todoCompleted = function (todo) {
-		if (todo.completed) {
-			$scope.remainingCount--;
-		} else {
-			$scope.remainingCount++;
-		}
-		todoStorage.put(todos);
-	};
+  $scope.todoCompleted = function (todo) {
+    if (todo.completed) {
+      $scope.remainingCount--;
+    } else {
+      $scope.remainingCount++;
+    }
+    todoStorage.put(todos);
+  };
 
-	$scope.clearCompletedTodos = function () {
-		$scope.todos = todos = todos.filter(function (val) {
-			return !val.completed;
-		});
-		todoStorage.put(todos);
-	};
+  $scope.clearCompletedTodos = function () {
+    $scope.todos = todos = todos.filter(function (val) {
+      return !val.completed;
+    });
+    todoStorage.put(todos);
+  };
 
-	$scope.markAll = function (completed) {
-		todos.forEach(function (todo) {
-			todo.completed = completed;
-		});
-		$scope.remainingCount = completed ? 0 : todos.length;
-		todoStorage.put(todos);
-	};
+  $scope.markAll = function (completed) {
+    todos.forEach(function (todo) {
+      todo.completed = completed;
+    });
+    $scope.remainingCount = completed ? 0 : todos.length;
+    todoStorage.put(todos);
+  };
 
-	/**
-	 * Need to be added for speech recognition
-	 */
-	
-	var findTodo = function(title){
-		for (var i=0; i<todos.length; i++){
-			if (todos[i].title === title) {
-				return todos[i];
-			}
-		}
-		return null;
-	};
+  /**
+   * Need to be added for speech recognition
+   */
+  
+  var findTodo = function(title){
+    for (var i=0; i<todos.length; i++){
+      if (todos[i].title === title) {
+        return todos[i];
+      }
+    }
+    return null;
+  };
 
-	var completeTodo = function(title){
-		for (var i=0; i<todos.length; i++){
-			if (todos[i].title === title) {
-				todos[i].completed = ! todos[i].completed;
-				$scope.todoCompleted(todos[i]);
-				$scope.safeApply();
-				return true;
-			}
-		}
-	};
+  var completeTodo = function(title){
+    for (var i=0; i<todos.length; i++){
+      if (todos[i].title === title) {
+        todos[i].completed = ! todos[i].completed;
+        $scope.todoCompleted(todos[i]);
+        $scope.safeApply();
+        return true;
+      }
+    }
+  };
 
   $scope.command = function(utterance) {
     $speechRecognition.command(utterance);
@@ -133,126 +133,126 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, todoStorage,
   };
 
   $scope.correctUtterance = function(utterance) {
-  	// TODO correctUtterance 
+    // TODO correctUtterance 
     $scope.lastUtterance = '';
   };
 
-	var LANG = 'en-US';
-	$speechRecognition.onstart(function(e){
-		$speechSynthetis.speak('Yes? How can I help you?', 'en-US');
-	});
-	$speechRecognition.onerror(function(e){
-		var error = (e.error || '');
-		alert('An error occurred ' + error);
-	});
-	// $speechRecognition.setLang(LANG);
-	$speechRecognition.listen();
+  var LANG = 'en-US';
+  $speechRecognition.onstart(function(e){
+    $speechSynthetis.speak('Yes? How can I help you?', 'en-US');
+  });
+  $speechRecognition.onerror(function(e){
+    var error = (e.error || '');
+    alert('An error occurred ' + error);
+  });
+  // $speechRecognition.setLang(LANG);
+  $speechRecognition.listen();
 
-	$speechRecognition.onUtterance(function(utterance){
-		$scope.lastUtterance = utterance;
-	});
+  $speechRecognition.onUtterance(function(utterance){
+    $scope.lastUtterance = utterance;
+  });
 
-	$scope.recognition = {};
-	$scope.recognition['en-US'] = {
-		'addToList': {
-			'regex': /^do .+/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				var parts = utterance.split(' ');
-				if (parts.length > 1) {
-					$scope.newTodo = parts.slice(1).join(' ');
-					$scope.addTodo();
-					$scope.safeApply();
-				}
-			}
-		},
-		'show-all': {
-			'regex': /show.*all/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				$location.path('/');
-				$scope.safeApply();
-			}
-		},
-		'show-active': {
-			'regex': /show.*active/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				$location.path('/active');
-				$scope.safeApply();
-			}
-		},
-		'show-completed': {
-			'regex': /show.*complete/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				$location.path('/completed');
-				$scope.safeApply();
-			}
-		},
-		'mark-all': {
-			'regex': /^mark/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				$scope.markAll(1);
-				$scope.safeApply();
-			}
-		},
-		'unmark-all': {
-			'regex': /^unmark/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				$scope.markAll(1);
-				$scope.safeApply();
-			}
-		},
-		'clear-completed': {
-			'regex': /clear.*/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				$scope.clearCompletedTodos();
-				$scope.safeApply();
-			}
-		},
-		'listTasks': [{
-			'regex': /^complete .+/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				var parts = utterance.split(' ');
-				if (parts.length > 1) {
-					completeTodo(parts.slice(1).join(' '));
-				}
-			}
-		},{
-			'regex': /^remove .+/gi,
-			'lang': 'en-US',
-			'call': function(utterance){
-				var parts = utterance.split(' ');
-				if (parts.length > 1) {
-					var todo = findTodo(parts.slice(1).join(' '));
-					console.log(todo);
-					if (todo) {
-						$scope.removeTodo(todo);
-						$scope.safeApply();
-					}
-				}
-			}
-		}]
-	};
+  $scope.recognition = {};
+  $scope.recognition['en-US'] = {
+    'addToList': {
+      'regex': /^do .+/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        var parts = utterance.split(' ');
+        if (parts.length > 1) {
+          $scope.newTodo = parts.slice(1).join(' ');
+          $scope.addTodo();
+          $scope.safeApply();
+        }
+      }
+    },
+    'show-all': {
+      'regex': /show.*all/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        $location.path('/');
+        $scope.safeApply();
+      }
+    },
+    'show-active': {
+      'regex': /show.*active/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        $location.path('/active');
+        $scope.safeApply();
+      }
+    },
+    'show-completed': {
+      'regex': /show.*complete/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        $location.path('/completed');
+        $scope.safeApply();
+      }
+    },
+    'mark-all': {
+      'regex': /^mark/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        $scope.markAll(1);
+        $scope.safeApply();
+      }
+    },
+    'unmark-all': {
+      'regex': /^unmark/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        $scope.markAll(1);
+        $scope.safeApply();
+      }
+    },
+    'clear-completed': {
+      'regex': /clear.*/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        $scope.clearCompletedTodos();
+        $scope.safeApply();
+      }
+    },
+    'listTasks': [{
+      'regex': /^complete .+/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        var parts = utterance.split(' ');
+        if (parts.length > 1) {
+          completeTodo(parts.slice(1).join(' '));
+        }
+      }
+    },{
+      'regex': /^remove .+/gi,
+      'lang': 'en-US',
+      'call': function(utterance){
+        var parts = utterance.split(' ');
+        if (parts.length > 1) {
+          var todo = findTodo(parts.slice(1).join(' '));
+          console.log(todo);
+          if (todo) {
+            $scope.removeTodo(todo);
+            $scope.safeApply();
+          }
+        }
+      }
+    }]
+  };
 
-	var ignoreUtterance = {};
-	ignoreUtterance['addToList'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['addToList']);
-	ignoreUtterance['show-all'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['show-all']);
-	ignoreUtterance['show-active'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['show-active']);
-	ignoreUtterance['show-completed'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['show-completed']);
-	ignoreUtterance['mark-all'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['mark-all']);
-	ignoreUtterance['unmark-all'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['unmark-all']);
-	ignoreUtterance['clear-completed'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['clear-completed']);
+  var ignoreUtterance = {};
+  ignoreUtterance['addToList'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['addToList']);
+  ignoreUtterance['show-all'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['show-all']);
+  ignoreUtterance['show-active'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['show-active']);
+  ignoreUtterance['show-completed'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['show-completed']);
+  ignoreUtterance['mark-all'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['mark-all']);
+  ignoreUtterance['unmark-all'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['unmark-all']);
+  ignoreUtterance['clear-completed'] = $speechRecognition.listenUtterance($scope.recognition['en-US']['clear-completed']);
 
-	/*
-		to ignore listener call returned function
-	 */
-	// ignoreUtterance['addToList']();
+  /*
+    to ignore listener call returned function
+   */
+  // ignoreUtterance['addToList']();
 });
 
 }());
